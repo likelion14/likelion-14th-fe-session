fcbc89cbd6023e4d13ad1bea1bca17415685743c
Yudam Jung <udamee1025@gmail.com>
2026-06-04 12:26:11 +0900

    [11주차] 정유담 과제 제출

---
+++ week11/src/App.js
@@ -0,0 +1,12 @@
+import MovieFavorites from './components/MovieFavorites';
+
+function App() {
+
+  return (
+    <div className="App">
+      <MovieFavorites />
+    </div>
+  );
+}
+
+export default App;
